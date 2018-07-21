9a5edc7bce316c1dbbe5b2ec47790cb98a94c59f
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2018-07-21 21:41:47 +0200

    chores - delete maps component

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,7 +5,6 @@ import { UserProfileComponent } from '../../user-profile/user-profile.component'
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
 import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 
 export const AdminLayoutRoutes: Routes = [
@@ -30,6 +29,5 @@ export const AdminLayoutRoutes: Routes = [
   },
   { path: 'typography', component: TypographyComponent },
   { path: 'icons', component: IconsComponent },
-  { path: 'maps', component: MapsComponent },
   { path: 'notifications', component: NotificationsComponent },
 ];
